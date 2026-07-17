54324b28663d5e802424aa8781fb6afa4ee71510
sempr3larouco <pablopetin2007@gmail.com>
2026-07-17 23:03:48 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,3 +53,15 @@ lightbox.addEventListener("click",(e)=>{
     }
 
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 80) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
